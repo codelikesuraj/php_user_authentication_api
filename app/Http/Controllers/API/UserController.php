f599666e9c0c6ecdeda1273034d673affe3f2ec4
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-08-01 14:34:56 +0100

    added password hashing when creating user

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,13 +5,22 @@ namespace App\Http\Controllers\API;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\API\LoginUserRequest;
 use App\Http\Requests\API\RegisterUserRequest;
 
 class UserController extends Controller
 {
     public function register(RegisterUserRequest $request)
     {
-        $user = User::create($request->validated());
+        $credentials = [
+            'name' => strtolower($request->validated('name')),
+            'email' => $request->validated('email'),
+            'password' => Hash::make($request->validated('password')),
+        ];
+
+        $user = User::create($credentials);
 
         return response()->json(['data' => $user], 201);
     }
