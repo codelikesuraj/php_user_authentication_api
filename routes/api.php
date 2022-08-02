6e79dea7e4c78961ff44e4a9a16ba05531d0008b
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-08-02 06:01:40 +0100

    updated routes and appserviceprovider

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
+// Register user
 Route::post('user/create', [UserController::class, 'register'])->name('user.register');
+
+// Login user
 Route::post('user/login', [UserController::class, 'login'])->name('user.login');
+
+// Update user
 Route::put('user/update/{id}', [UserController::class, 'update'])->name('user.update');
+
+// Delete user
 Route::delete('user/delete/{id}', [UserController::class, 'delete'])->name('user.delete');
+
+// Get a user
 Route::get('user/{id}', [UserController::class, 'getUser'])->name('user.one');
+
+// Get all users
 Route::get('users', [UserController::class, 'getUsers'])->name('user.all');
